37b6d0c3c3bede8802bec970c1b2ae6422599f64
Roleen <newnessroleen@yahoo.com>
2014-10-18 10:11:41 -0400

    First Commit
    Added Unity Project to github

---
+++ ZooParade/Assets/ZooParade/Scripts/CameraSetup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSetup : MonoBehaviour {
+
+	public static int cameraZ = -10;
+
+	void Awake()
+	{
+		//camera.orthographicSize = Screen.height * 1 / 2;
+		Screen.fullScreen = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
