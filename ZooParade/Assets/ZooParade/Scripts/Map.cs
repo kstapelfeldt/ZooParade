7814dae89e07e6c8fa761ea41eb4bed13bc1769d
Roleen <newnessroleen@yahoo.com>
2015-01-09 16:09:48 -0500

    Player, Animal and Map Class
    Created Player and Animal Classes and outline of Map Class

---
+++ ZooParade/Assets/ZooParade/Scripts/Map.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map
+{
+	public string name;
+	public List<Animal> animals = new List<Animal> ();
+	public List <Checkpoint> checkpoints;
+	public Player player;
+
+	public Map(string name, List<Animal> animals, List<Checkpoint> checkpoints)
+	{
+		this.name = name;
+		this.animals = animals;
+		this.checkpoints = checkpoints;
+	}
+
+	public void AssignPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	public Checkpoint GetNextCheckPoint()
+	{
+		// Player has the current checkpoint
+		return new Checkpoint (0, 0, true, false, false, new Checkpoint[]{});
+	}
+
+	public void MovePlayer()
+	{
+		// Use GetNextCheckPoint to get the next checkpoint
+		// Move the Player to the next Checkpoint using MovePlayer()
+	}
+}
---
+++ ZooParade/Assets/ZooParade/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player
+{
+	public string name;
+	public Map map;
+	public List<Checkpoint> checkpoints = new List<Checkpoint> ();
+	public List<Animal> animalsCaptured = new List<Animal> ();
+
+	public Checkpoint currentCheckpoint;
+
+	public Player(string name, Map map)
+	{
+		this.name = name;
+		this.map = map;
+	}
+
+	public void MovePlayer(Checkpoint checkpoint)
+	{
+		currentCheckpoint = checkpoint;
+	}
+}
