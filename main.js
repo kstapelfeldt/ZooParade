22b8954cce4b3f467fbfe5043f9e35ac4b8ca255
roleen-mac <Roleen@wifihost-140-137.wireless.utsc.utoronto.ca>
2015-06-04 17:16:38 -0400

    Minor change

---
+++ main.js
@@ -0,0 +1,33 @@
+var documentWidth = $(document).width();
+var documentHeight = $(document).height();
+var initialWindowWidth = $(window).width();
+var initialWindowHeight = $(window).height();
+
+var leftMap = SVG('leftMap');
+
+var circle = leftMap.circle("6%").attr({ fill: 'green' });
+
+circle.translate(GetMapWidth() * 0.5, GetMapHeight() * 0.5);
+
+var circle2 = leftMap.circle("6%").attr({ fill: 'green' });
+
+circle2.translate(GetMapWidth() * 0.4, GetMapHeight() * 0.4);
+
+
+function GetMapWidth(){
+	return documentWidth * 0.35;
+}
+
+
+function GetMapHeight(){
+	return documentHeight * 0.70;
+}
+
+
+
+/* Algorithm: 
+ * Make an array of the relative position of coordinates
+ * Function to create circles at the coordinates and draw rectangles between two centers
+ * Populate checkpoints at the same time
+ * 
+ */
