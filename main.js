219e90e29b62b6d0e6aadc70692cd6fae6264021
roleen-mac <Roleen@wifihost-140-137.wireless.utsc.utoronto.ca>
2015-06-05 14:51:23 -0400

    Commented main.js

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ var documentHeight = $(document).height();
 var initialWindowWidth = $(window).width();
 var initialWindowHeight = $(window).height();
 
-var mapScale = 0.097;
+var mapScale = 0.097;			// Scale the maps
+var moveLeftMap = 0;			// Move the left map horizontally
+var moveRightMap = 0;			// Move the right map horizontally
+var moveVertical = 0;			// Move the maps vertically
 
+// Relative position of all the checkpoints
 var positions = [[2, 1.2], [3, 1.2], 
 			[4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2.5],
 			[4, 3], [4.7, 3.2], [9.5, 3.3],
@@ -16,30 +20,35 @@ var positions = [[2, 1.2], [3, 1.2],
 			[4.4, 8.4], [5.3, 8.6], [9.4, 8.4],
 			[4, 9.5], [5, 9.5], [6, 9.5], [7, 9.5], [8, 9.5], [9, 9]];
 
+// Booleans representing the capture point status of every checkpoint
 var capturePoints = [false, false, false, false, false, true, false, false, false, false, 
 					false, false, true, false, false, false, false, false, true, false, 
 					true, false, false, false, false, false, true, false, false, false, 
 					false, false, false, false, false, false, false, false, true, false, 
 					false, false];
 
+// Booleans representing the greenS point status of every checkpoint
 var greenSPoints = [false, false, false, false, false, false, false, false, false, false, 
 					true, false, false, false, true, false, false, false, false, false, 
 					false, false, false, false, true, false, false, false, false, true, 
 					false, false, false, true, false, false, false, false, false, false, 
 					false, true];
 
+// Booleans representing the redS point status of every checkpoint
 var redSPoints = 	[false, false, true, false, false, false, false, false, false, false, 
 					false, false, false, false, false, true, false, false, false, false, 
 					false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false, false, false, false, false, false, 
 					false, false];
 
+// Booleans representing the hazard point status of every checkpoint
 var hazardPoints = [false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false, false, false, false, false, true, 
 					false, false, false, false, false, false, false, false, false, false, 
 					true, false, false, false, false, false, false, false, false, false, 
 					false, false];
 
+// All the edges in the maps
 var pathEdges = [[0 ,1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
 				[7, 10], [10, 16], [16, 22], [22, 26], [26, 32], [32, 35], [35, 41],
 				[41, 40], [40, 39], [39, 38], [38, 37], [37, 36], [36, 33], [33, 27],
@@ -48,32 +57,40 @@ var pathEdges = [[0 ,1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7],
 				[33, 28], [28, 25], [25, 20], [20, 21], [21, 16], [37, 34], [34, 29], 
 				[29, 30], [30, 31], [31, 32]];
 
-
+// SVG objects for Left map
 var leftMap = SVG('leftMap');
 var leftPath = leftMap.nested();
 var leftCheckpoints = new Array();
 
+// SVG objects for Right map
 var rightMap = SVG('rightMap');
 var rightPath = rightMap.nested();
 var rightCheckpoints = new Array();
 
-
+// Creates Left Map
 CreateMapCheckpoints(positions, capturePoints, greenSPoints, redSPoints, hazardPoints, leftMap, false);
-CreateMapCheckpoints(positions, capturePoints, greenSPoints, redSPoints, hazardPoints, rightMap, true);
-
 LinkCheckpoints(pathEdges, leftPath, false);
+
+// Creates Right Map
+CreateMapCheckpoints(positions, capturePoints, greenSPoints, redSPoints, hazardPoints, rightMap, true);
 LinkCheckpoints(pathEdges, rightPath, true);
 
 
+
+/* Creates the checkpoints for a map at the given positions and sets the status of capture point,
+ * green S point, red S point and hazard point for each checkpoint for the given map
+ * Parameter types: (list of list, list of boolean, list of boolean, list of boolean, 
+ *                   list of boolean, Map, boolean)
+ */
 function CreateMapCheckpoints(positions, capturePoints, greenSPoints, redSPoints, hazardPoints, map, right){
 	for (i = 0; i < positions.length; i++){
 		if (right){
-			x = GetMapWidth() - (GetMapWidth() * positions[i][0] * mapScale);
+			x = (GetMapWidth() - (GetMapWidth() * positions[i][0] * mapScale)) + (GetMapWidth() * moveRightMap);
 		} else{
-			x = GetMapWidth() * positions[i][0] * mapScale;
+			x = (GetMapWidth() * positions[i][0] * mapScale) + (GetMapWidth() * moveLeftMap);
 		}
 		
-		y = GetMapHeight() * positions[i][1] * mapScale;
+		y = (GetMapHeight() * positions[i][1] * mapScale) + (GetMapHeight() * moveVertical);
 
 		var pointColor = 'green';
 		if (capturePoints[i]){
@@ -97,6 +114,9 @@ function CreateMapCheckpoints(positions, capturePoints, greenSPoints, redSPoints
 }
 
 
+/* Links all the checkpoints in the give map based on the edges for the given map
+ * Parameter types: (list of list, Map, boolean)
+ */
 function LinkCheckpoints(edges, map, right){
 	for (i = 0; i < edges.length; i++){
 		index1 = edges[i][0];
@@ -119,11 +139,12 @@ function LinkCheckpoints(edges, map, right){
 }
 
 
+/* Returns the width of a map */
 function GetMapWidth(){
 	return documentWidth * 0.35;
 }
 
-
+/* Returns the height of a map */
 function GetMapHeight(){
 	return documentHeight * 0.70;
 }
