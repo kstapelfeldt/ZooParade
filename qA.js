7424eb63eee633154b3e4afe1e75074308ca6efa
munozalvalu <munozalvalu@gmail.com>
2015-06-17 17:44:09 -0400

    Changed the values

--- qA.js
+++ qA.js
@@ -2,48 +2,110 @@
 *We will need a separate array for the AI.
 *Each time this array will hold only the current questions revelant to one animal.
 */
-var values = [];
+var playerQuestions = [];
+var AIQUestions = [];
 /*
 *Once a question has been used it will be taken out and be put into this array.
 *If all questions have been exhausted the original array will be filled with this one.
 */
-var usedValues = [];
+var AIUsed = [];
+var playerUsed = [];
 
-/*Takes in the parsed values of the file
-*then seperates the 
-*/
-function ProcessQA(results) {
-	for (count = 0; count < results.data.length; count++){
-		values.push(results.data[count]);	
-	}
-	GetNextQuestionAnswer(1);
-}
 /*
 *Is given the type of question desired to be returned.
 *Returns a random question of the requested type that has not already been used.
 *unless the questions have been exhausted. Then techiniquely they have been used.
+*Takes in a bool player, to determine which array to put
+*the questions in. If player is false it goes into the
+*AIQuestions array, if true goes to PlayerQuestions array.
 *Parameter type: (String)
 *return: List object with [boolean, int, string, string]
 */
-function GetNextQuestionAnswer(QuestionType){
-	var tempHold = [];
-	alert(tempHold);
-	for(count<0; count< values.length; count++){
-		if(values[count][QuestionType.parseInt()] == 1){
-			tempHold.push(values[count].pop());
+function GetNextQuestionAnswer(QuestionType, player){
+	var result = [];
+	var isIn;
+	//isEmpty(player);
+	if(player){
+		for(count = 0; count < playerQuestions.data.length-1; count++){
+			alert(playerQuestions.data[count]["Question"]);
+			/*if(playerQuestions.data[count]["QuestionType"] == QuestionType){
+				/*for i in playerUsed.data{
+					if(playerQuestions.data[count] == i){
+						isIn = true;
+					}
+				}
+
+				//if(!isIn){
+					//result will collect all the values of the wanted question type.
+					//don't add something to result if you have already used it.
+					//something here is messing with the indexs and deleting the wrong thing.
+					result.push(playerQuestions.data[count]);
+					delete playerQuestions.data[count];
+					playerQuestions.data.splice(count,1);	
+				//}
+			}*/
+			//alert(playerQuestions.data[count]["Question"]+ "after");
+		}	
+		alert(playerQuestions.data.length);
+		//alert(result.length);
+		playerUsed.push(result[Math.floor(Math.random() * result.length)]);
+		//alert(playerUsed[0]["Question"]);
+
+		return playerUsed;
+		//get a random question from this selection
+	}else{
+		for(count = 0; count < AIQuestions.data.length; count++){
+			if(AIQuestions.data[count]["QuestionType"] == QuestionType){
+				result.push(AIQuestions.data[count]["Question"]);
+				delete AIQuestions.data[count]["QuestionType"];
+				AIQuestions.data.splice(count,1);
+			}
 		}
 	}
-	alert(tempHold);
 }
-/*Reads the contents of the file parses them,
-*then sends the results to be processed
-*by ProcessQA
-*Parameter type: (string), (config)
+
+/*Check if all the questions have been used for either the player or AI
+*if they have then reload all the questions from either AIUsed or playerUsed
+*so that you can use them again.
+*Takes in which player is being asked to reload.
+*Parameter type: (bool)
+*/
+function isEmpty(Player){
+	if(player){
+		if(playerQuestions.data.length <= 0){
+			PlayerQuestions.data = playerUsed.data;
+			playerUsed.data.length = 0; 
+		}
+	}else{
+		if(AIQuestions.data.length <= 0){
+			AIQuestions.data = AIUsed.data;
+			AIUsed.data.length = 0;
+		}
+	}
+}
+/*Reads the contents of the file and parses it.
+*Takes in a bool player, to determine which array to put
+*the questions in. If player is false it goes into the
+*AIQuestions array, if true goes to PlayerQuestions array.
+*Parameter type: (string), (bool)
 *Callback runs ProcessQA
 */
 
-function ReadFile(path) {
-    Papa.parse(path, { download: true, skipEmptyLines: true, dynamicTyping: true, complete: function (results) { ProcessQA(results); } });
+function ReadFile(path, player) {
+    Papa.parse(path+ ".csv", { 
+	    download: true,
+	    skipEmptyLines: true,
+	    header: true,
+	    complete: function (results) {
+	    	if(player){
+	    		playerQuestions = results;
+	    		GetNextQuestionAnswer(1,player);
+	    	}
+	    	else{
+	    		AIQuestions = results;
+	    	}
+	    }
+ 	});
 }
 
-ReadFile("Dog.csv");
+ReadFile("Dog", true);
