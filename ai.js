72f98623c322b86110c0d645e7264f5ea8271311
roleen-mac <Roleen@wifihost-143-151.wireless.utsc.utoronto.ca>
2015-07-16 16:21:20 -0400

    AI partially implemented

--- ai.js
+++ ai.js
@@ -9,11 +9,17 @@ function AIMove(){
 		var rand = Math.floor(Math.random() * 7);
 		if (rand <= 5){
 			// Correct Answer Move
+			CorrectAnswerMove();
+			if (player.possiblePaths != null && player.possiblePaths.length > 1){
+				// Choose a path
+			}
 		} else {
 			// Wrong Answer Move
+			WrongAnswerMove();
 		}
 	} else if (player.currentCheckpoint.redS || player.currentCheckpoint.greenS){
 		// Spin the spinner
+		Spin();
 
 		// Wait for the spinner to stop spinning
 
